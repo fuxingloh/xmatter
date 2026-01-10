c14c6a3c5325cde9f7d25ae2857479ed2c76b332
Fuxing <loh@fuxing.dev>
2026-01-11 01:09:21 +0800

    chore: replace jest with vitest

--- workspace/jest-preset/jest-preset.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testRegex: ".*\\.test\\.ts$",
-  reporters: ["default", "github-actions"],
-  moduleFileExtensions: ["ts", "js", "mjs", "cjs", "json"],
-  transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
-  },
-};
--- workspace/sync/src/README.test.ts
+++ workspace/sync/src/README.test.ts
@@ -1,4 +1,4 @@
-import { expect, it } from "@jest/globals";
+import { expect, it } from "vitest";
 
 import { validate } from "./README";
 
