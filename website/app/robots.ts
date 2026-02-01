0a4b1a26b481c7cdbfc8a2c0edfcd858c5d37c1a
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-02 01:43:15 +0800

    chore: googlebot robots

--- website/app/robots.ts
+++ website/app/robots.ts
@@ -2,11 +2,17 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/_next/", "/_vercel/"],
-    },
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: ["/_next/", "/_vercel/"],
+      },
+      {
+        userAgent: "*",
+        disallow: "/",
+      },
+    ],
     sitemap: `https://xmatter.org/sitemap.xml`,
   };
 }
