f91607a376071362fced0c6636135117c5083f47
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-03 23:38:41 +0800

    chore: refactor header

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -25,31 +25,42 @@ export default function RootLayout(props: { children: ReactNode }): ReactElement
       <head />
       <body className="bg-mono-50 text-mono-950">
         <ThemeProvider>
-          <header className="border-mono-200 border-b py-3">
-            <nav className="mx-auto flex w-full max-w-7xl items-center justify-between px-5">
-              <ActiveLink
-                href="/"
-                mode="exact"
-                className="hover:bg-invert/5 -mx-3 -my-1 flex items-center gap-1.5 rounded px-3 py-1"
-                activeClassName="!cursor-default hover:!bg-transparent"
-              >
-                <Xmatter className="size-4.5" />
-                <div className="text-lg font-bold">Xmatter</div>
-              </ActiveLink>
-              <Link
-                href="https://github.com/fuxingloh/xmatter"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="-mx-3 -my-1 flex items-center gap-1.5 rounded px-3 py-1"
-              >
-                <GitHub className="size-4.5" />
-              </Link>
-            </nav>
-          </header>
+          <Header />
           {props.children}
         </ThemeProvider>
         <Analytics />
       </body>
     </html>
+  );
+}
+
+function Header() {
+  return (
+    <header className="border-mono-200 border-b py-3">
+      <nav className="mx-auto flex w-full max-w-7xl items-center justify-between px-5">
+        <ActiveLink
+          href="/"
+          mode="exact"
+          className="hover:bg-invert/5 -mx-3 -my-1 flex items-center gap-1.5 rounded px-3 py-1"
+          activeClassName="!cursor-default hover:!bg-transparent"
+        >
+          <Xmatter className="size-4.5" />
+          <div className="text-lg font-bold">Xmatter</div>
+        </ActiveLink>
+
+        <div className="flex items-center gap-4">
+          <div>Docs</div>
+          <div>Theme Switch</div>
+          <Link
+            href="https://github.com/fuxingloh/xmatter"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="-mx-3 -my-1 flex items-center gap-1.5 rounded px-3 py-1"
+          >
+            <GitHub className="size-4.5" />
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
