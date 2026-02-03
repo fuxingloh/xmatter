12016e243b99b8a097f8c920dd8d8bfcd437327d
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-04 02:05:38 +0800

    chore: new icon

--- website/app/eip155/[chainId]/[address]/page.tsx
+++ website/app/eip155/[chainId]/[address]/page.tsx
@@ -94,10 +94,11 @@ export default async function Page(props: PageProps<"/eip155/[chainId]/[address]
 
           <div>
             <div className="mb-2 flex">
-              <h4 className="text-mono-950 bg-mono-100 rounded-sm px-1.5 py-1.5 font-mono text-sm select-all">
-                <span className="select-none">`</span>
+              <h4 className="text-mono-950 bg-mono-200/50 flex items-center gap-1.5 rounded-sm px-2 py-1.25 font-mono text-sm select-all">
+                <svg viewBox="0 0 24 24" width="16" height="16">
+                  <path d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113z" />
+                </svg>
                 <span>npm install xmatter</span>
-                <span className="select-none">`</span>
               </h4>
             </div>
           </div>
