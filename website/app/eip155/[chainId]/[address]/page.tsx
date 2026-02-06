2f220a31abd4bf6d4bb682b2f1ae9e89ac378a4e
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-06 09:47:09 +0800

    chore: refactor Npm

--- website/app/eip155/[chainId]/[address]/page.tsx
+++ website/app/eip155/[chainId]/[address]/page.tsx
@@ -7,6 +7,7 @@ import { IconsTab } from "@/app/eip155/[chainId]/[address]/IconsTab";
 import { FrontmatterLink } from "@/app/eip155/[chainId]/[address]/FrontmatterLink";
 import { IdentifierSelect } from "@/app/eip155/[chainId]/[address]/IdentifierSelect";
 import Link from "next/link";
+import Npm from "@/app/eip155/[chainId]/[address]/Npm";
 
 export async function generateStaticParams() {
   return [
@@ -98,19 +99,7 @@ export default async function Page(props: PageProps<"/eip155/[chainId]/[address]
             <Markdown>{content}</Markdown>
           </div>
 
-          <div>
-            <div className="mb-2 flex">
-              <h4 className="text-mono-950 bg-mono-200/50 flex items-center gap-1.5 rounded-sm px-2 py-1.25 font-mono text-sm select-all">
-                <svg viewBox="0 0 24 24" width="16" height="16">
-                  <path
-                    fill="currentColor"
-                    d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113z"
-                  />
-                </svg>
-                <span>npm install xmatter</span>
-              </h4>
-            </div>
-          </div>
+          <Npm />
         </div>
       </main>
 
