23f8cf6091faeb9f98701a980b1bd75441204c9e
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-08 16:57:37 +0800

    chore: missing ids

--- website/app/eip155/[chainId]/[address]/page.tsx
+++ website/app/eip155/[chainId]/[address]/page.tsx
@@ -20,6 +20,15 @@ export async function generateStaticParams() {
 
 const chains: Record<string, string> = {
   "1": "Ethereum",
+  "10": "Optimism",
+  "56": "Binance Smart Chain",
+  "137": "Polygon",
+  "8453": "Base",
+  "42161": "Arbitrum One",
+  "42220": "Celo",
+  "43114": "Avalanche C-Chain",
+  "11155111": "Ethereum Sepolia",
+  "1313161554": "Aurora",
 };
 
 export async function generateMetadata(props: PageProps<"/eip155/[chainId]/[address]">): Promise<Metadata> {
