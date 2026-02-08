9913a79f370ce96ff956a0d1176aafb4fd6d56b3
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-08 16:33:07 +0800

    chore: setup

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -16,13 +16,16 @@ export default function Page() {
 
   return (
     <main className="mx-auto w-full max-w-7xl px-5 pt-8 pb-48">
-      <div className="py-8">
-        <h1 className="text-3xl font-semibold">Xmatter</h1>
-        <p className="text-mono-700 mt-2 text-lg">
-          Structured metadata for address and smart contracts.
-          <br />
-          The <RollingText /> registry for assets on-chain.
-        </p>
+      <div className="grid grid-cols-1 gap-y-12 py-8 lg:grid-cols-2 lg:gap-x-12 lg:gap-y-0">
+        <div>
+          <h1 className="text-3xl font-semibold">Xmatter</h1>
+          <p className="text-mono-700 mt-2 text-lg">
+            Structured metadata for address and smart contracts.
+            <br />
+            The <RollingText /> registry for assets on-chain.
+          </p>
+        </div>
+        <div></div>
       </div>
 
       <div className="mt-32">
