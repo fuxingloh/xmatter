9291a12e10283e5b59e6c4670548d9ade75be9ca
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-10 01:30:25 +0800

    chore: map all agent-trust-wallet

--- packages/agent-trust-wallet/agent.ts
+++ packages/agent-trust-wallet/agent.ts
@@ -86,6 +86,42 @@ function getStandards(type: string): string[] {
     case "AURORA":
     case "CELO":
     case "BASE":
+    case "FANTOM":
+    case "ETC20":
+    case "MANTLE":
+    case "SONIC":
+    case "BLAST":
+    case "MONAD":
+    case "TT20":
+    case "HRC20":
+    case "MERLIN":
+    case "SCROLL":
+    case "METER":
+    case "KRC20":
+    case "XDAI":
+    case "ZKSYNC":
+    case "PLASMA":
+    case "LINEA":
+    case "TRC21":
+    case "MEGAETH":
+    case "BOUNCEBIT":
+    case "ZKEVM":
+    case "GO20":
+    case "WAN20":
+    case "ZKLINK":
+    case "MOONBEAM":
+    case "OPBNB":
+    case "KIP20":
+    case "RONIN":
+    case "KAIA":
+    case "POA20":
+    case "CONFLUX":
+    case "KAVAEVM":
+    case "MANTA":
+    case "MOONRIVER":
+    case "ACALAEVM":
+    case "CRC20":
+    case "ZETACHAIN":
       return ["erc20"];
     case "TRC10":
       return ["trc10"];
@@ -143,6 +179,181 @@ await agent.walk(".repo/blockchains/celo/assets", {
 await agent.walk(".repo/blockchains/base/assets", {
   filter: (data) => data.type === "BASE",
   toUri: (data) => `eip155/8453/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/fantom/assets", {
+  filter: (data) => data.type === "FANTOM",
+  toUri: (data) => `eip155/250/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/classic/assets", {
+  filter: (data) => data.type === "ETC20",
+  toUri: (data) => `eip155/61/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/mantle/assets", {
+  filter: (data) => data.type === "MANTLE",
+  toUri: (data) => `eip155/5000/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/sonic/assets", {
+  filter: (data) => data.type === "SONIC",
+  toUri: (data) => `eip155/146/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/blast/assets", {
+  filter: (data) => data.type === "BLAST",
+  toUri: (data) => `eip155/81457/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/monad/assets", {
+  filter: (data) => data.type === "MONAD",
+  toUri: (data) => `eip155/143/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/thundertoken/assets", {
+  filter: (data) => data.type === "TT20",
+  toUri: (data) => `eip155/108/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/heco/assets", {
+  filter: (data) => data.type === "HRC20",
+  toUri: (data) => `eip155/128/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/merlin/assets", {
+  filter: (data) => data.type === "MERLIN",
+  toUri: (data) => `eip155/4200/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/scroll/assets", {
+  filter: (data) => data.type === "SCROLL",
+  toUri: (data) => `eip155/534352/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/meter/assets", {
+  filter: (data) => data.type === "METER",
+  toUri: (data) => `eip155/82/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/kcc/assets", {
+  filter: (data) => data.type === "KRC20",
+  toUri: (data) => `eip155/321/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/xdai/assets", {
+  filter: (data) => data.type === "XDAI",
+  toUri: (data) => `eip155/100/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/zksync/assets", {
+  filter: (data) => data.type === "ZKSYNC",
+  toUri: (data) => `eip155/324/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/plasma/assets", {
+  filter: (data) => data.type === "PLASMA",
+  toUri: (data) => `eip155/9745/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/linea/assets", {
+  filter: (data) => data.type === "LINEA",
+  toUri: (data) => `eip155/59144/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/megaeth/assets", {
+  filter: (data) => data.type === "MEGAETH",
+  toUri: (data) => `eip155/4326/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/bouncebit/assets", {
+  filter: (data) => data.type === "BOUNCEBIT",
+  toUri: (data) => `eip155/6001/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/polygonzkevm/assets", {
+  filter: (data) => data.type === "ZKEVM",
+  toUri: (data) => `eip155/1101/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/gochain/assets", {
+  filter: (data) => data.type === "GO20",
+  toUri: (data) => `eip155/60/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/wanchain/assets", {
+  filter: (data) => data.type === "WAN20",
+  toUri: (data) => `eip155/888/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/zklink/assets", {
+  filter: (data) => data.type === "ZKLINK",
+  toUri: (data) => `eip155/810180/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/moonbeam/assets", {
+  filter: (data) => data.type === "MOONBEAM",
+  toUri: (data) => `eip155/1284/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/opbnb/assets", {
+  filter: (data) => data.type === "OPBNB",
+  toUri: (data) => `eip155/204/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/okc/assets", {
+  filter: (data) => data.type === "KIP20",
+  toUri: (data) => `eip155/66/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/ronin/assets", {
+  filter: (data) => data.type === "RONIN",
+  toUri: (data) => `eip155/2020/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/klaytn/assets", {
+  filter: (data) => data.type === "KAIA",
+  toUri: (data) => `eip155/8217/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/poa/assets", {
+  filter: (data) => data.type === "POA20",
+  toUri: (data) => `eip155/99/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/cfxevm/assets", {
+  filter: (data) => data.type === "CONFLUX",
+  toUri: (data) => `eip155/1030/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/kavaevm/assets", {
+  filter: (data) => data.type === "KAVAEVM",
+  toUri: (data) => `eip155/2222/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/manta/assets", {
+  filter: (data) => data.type === "MANTA",
+  toUri: (data) => `eip155/169/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/moonriver/assets", {
+  filter: (data) => data.type === "MOONRIVER",
+  toUri: (data) => `eip155/1285/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/acalaevm/assets", {
+  filter: (data) => data.type === "ACALAEVM",
+  toUri: (data) => `eip155/787/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/cronos/assets", {
+  filter: (data) => data.type === "CRC20",
+  toUri: (data) => `eip155/25/${data.id.toLowerCase()}`,
+});
+
+await agent.walk(".repo/blockchains/zetachain/assets", {
+  filter: (data) => data.type === "ZETACHAIN",
+  toUri: (data) => `eip155/7000/${data.id.toLowerCase()}`,
 });
 
 await agent.walk(".repo/blockchains/tron/assets", {
